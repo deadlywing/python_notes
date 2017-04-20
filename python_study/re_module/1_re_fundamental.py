3eb028ed925b7f1730f2f6da4b4fe2d2b640591f
deadlywing <437743165@qq.com>
2017-04-20 17:26:36 +0800

    re_fundamental

--- python_study/re_module/1_re_fundamental.py
+++ python_study/re_module/1_re_fundamental.py
@@ -1,4 +1,21 @@
 """
 re fundamental knowledge test
+source : https://docs.python.org/3.5/library/re.html#re.X
+
+1. re.compile(pattern, flags=0)
+flag:
+re.A --- re.ASCII
+re.DEBUG
+re.I --- re.IGNORECASE
+re.M --- re.MULTILINE
+re.S --- re.DOTALL
+re.X ---- re.VERBOSE
+
+prog = re.compile(pattern)
+result = prog.match(string)
+is equivalent to
+result = re.match(pattern, string)
+
 """
+
 
